f0ec7d8fbdc41c42f8bcfa8e795e27cc3bb82418
BANGWON-LEE <qkddnjs31@gmail.com>
2023-03-27 10:38:46 +0900

    20230327 비 줄기 자연스럽게 하기 위한 작업

--- rain_wind/rainWind.js
+++ rain_wind/rainWind.js
@@ -16,6 +16,10 @@ let value =
       x_value : 170,
       y_value : 128  
    },
+   {
+      x_value : 150,
+      y_value : 128  
+   },
 ]
 
 // 화면 가까이에 내리는 rain
@@ -24,6 +28,7 @@ let nearRain = new Array(7).fill({x:value[0].x_value, y: value[0].y_value });
 let farRain = new Array(7).fill({x:value[1].x_value, y: value[1].y_value });
 let nearRain02 = new Array(7).fill({x:value[2].x_value, y: value[2].y_value });
 let farRain02 = new Array(7).fill({x:value[3].x_value, y: value[3].y_value });
+let farRain03 = new Array(7).fill({x:value[4].x_value, y: value[4].y_value });
 
 let canvas;
 
@@ -35,28 +40,27 @@ function setup() {
 function draw() {
 
       background(220);
-      let r =  Math.floor(Math.random() * 5); 
-      // point(nearRain[i].x ,nearRain[i].y)
+
+      let j = -30;
       for(let i = 0; i<nearRain.length; i++){
-         // let r = floor(random(5));
-      
-         // let r = Math.floor(Math.random() * 10) + 1;
+         
          let z = 0;
          z = i*175.7
-         // let s = 0;
-         // s = r+i;
+         j = j * (-1) 
 
          stroke(0,0,0);
-         strokeWeight(4.3);
-         ellipse(nearRain[i].x+z, nearRain[i].y  , 1, 46);
+         strokeWeight(2.9);
+         ellipse(nearRain[i].x+z, nearRain[i].y + j  , 1, 46);
          strokeWeight(1.6);
-         ellipse(farRain[i].x+z, farRain[i].y  , 1, 46);
-         strokeWeight(4.3);
-         ellipse(nearRain02[i].x+z, nearRain02[i].y  , 1, 38);
-         strokeWeight(4.3);
-         ellipse(nearRain02[i].x+z, nearRain02[i].y  , 1, 38);
+         ellipse(farRain[i].x+z, farRain[i].y + j  , 1, 46);
+         strokeWeight(2.9);
+         ellipse(nearRain02[i].x+z, nearRain02[i].y + j , 1, 38);
+         strokeWeight(2.9);
+         ellipse(nearRain02[i].x+z, nearRain02[i].y + j  , 1, 38);
          strokeWeight(2.1);
-         ellipse(farRain02[i].x+z, farRain02[i].y  , 1, 38);
+         ellipse(farRain02[i].x+z, farRain02[i].y + j  , 1, 38);
+         strokeWeight(2.1);
+         ellipse(farRain03[i].x+z, farRain03[i].y + j , 1, 38);
       }
       raining();
    
@@ -66,11 +70,15 @@ function draw() {
 let speed = [
    {
       xspeed : 0,
-      yspeed : 0.32
+      yspeed : 0.41
    },
    {
       xspeed : 0,
       yspeed : 0.52
+   },
+   {
+      xspeed : 0,
+      yspeed : 0.58
    },
 ]
 
@@ -79,11 +87,13 @@ function raining(){
 
    for(let i = 0; i<nearRain.length; i++){
       let r =  Math.floor(Math.random() * 3); 
+ 
 
       nearRain[i].y = (nearRain[i].y)+speed[0].yspeed
       nearRain02[i].y = (nearRain02[i].y)+speed[0].yspeed
       farRain[i].y = (farRain[i].y)+speed[1].yspeed
       farRain02[i].y = (farRain02[i].y)+speed[1].yspeed
+      farRain03[i].y = (farRain03[i].y)+speed[2].yspeed
 
       if((nearRain[i].x > width) || (nearRain[i].x < 0)){
          nearRain[r].x = 18
@@ -110,6 +120,11 @@ function raining(){
       }
 
       if((farRain02[i].y > height) || (farRain02[i].y > 310)){
+         // speed[i].yspeed = speed[i].yspeed * -1;
+         farRain02[r].x = value[3].x_value
+         farRain02[r].y = value[3].y_value
+      }
+      if((farRain02[i].y > height) || (farRain02[i].y > 290)){
          // speed[i].yspeed = speed[i].yspeed * -1;
          farRain02[r].x = value[3].x_value
          farRain02[r].y = value[3].y_value
